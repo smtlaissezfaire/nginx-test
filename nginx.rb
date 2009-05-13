9c896eb1c9160760d79d0ce2b2d519d27f9d5368
Scott Taylor <scott@railsnewbie.com>
2009-05-13 03:55:09 -0400

    Initial extration from flavorpill

---
+++ nginx.rb
@@ -0,0 +1,82 @@
+require "net/http"
+
+module Nginx
+  def server
+    @server || "http://localhost:4000"
+  end
+
+  attr_writer :server
+
+  def get(route)
+    @response = Curler.get route, server
+  end
+
+  def response
+    @response
+  end
+
+  def redirect_to(route)
+    RedirectMatcher.new(route)
+  end
+
+  class RedirectMatcher
+    def initialize(expected)
+      @expected = expected
+    end
+
+    def matches?(response)
+      uri = URI.parse(response.redirect_url)
+      @given = uri.path
+      
+      @expected == @given
+    end
+    
+    def failure_message
+      "Expected route '#{@expected}' to match given route '#{@given}'"
+    end
+  end
+
+  class Curler
+    def self.get(route, server)
+      obj = new(server)
+      obj.get(route)
+      obj.response
+    end
+
+    def initialize(server)
+      @server = server
+    end
+
+    def get(route)
+      uri = URI.parse(@server)
+      http = Net::HTTP.new(uri.host, uri.port)
+      @output = http.request_head route
+    end
+
+    def response
+      Response.new(@output)
+    end
+
+    class Response
+      def initialize(output)
+        @output = output
+      end
+
+      def success?
+        @output.is_a?(Net::HTTPOK)
+      end
+
+      def redirect?
+        @output.is_a?(Net::HTTPRedirection)
+      end
+
+      def permanent_redirect?
+        @output.is_a?(Net::HTTPMovedPermanently)
+      end
+
+      def redirect_url
+        @output['location']
+      end
+    end
+  end
+end
